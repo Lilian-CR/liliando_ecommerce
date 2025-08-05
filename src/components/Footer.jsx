fff12739958f2c390f817d8c462c4f40780eccd5
Lilian-CR <lilianxxx@gmail.com>
2025-08-05 03:22:52 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-primary/90 text-white text-xs md:text-sm py-4 px-4 md:px-4 leading-relaxed">
   <div className="max-w-6xl mx-auto text-center break-words">
-    © 2025 <strong>Lilian Rodrigues</strong> – Styled with custom Tailwind colours · Structured with reusable components · Bundled fast with <strong>Vite</strong> | ReDi School Berlin – FullStack Spring Semester 2025
+    © 2025 <strong>Lilian Rodrigues</strong> – Styled with custom Tailwind · Structured with reusable components · Bundled fast with <strong>Vite</strong> | ReDi School Berlin – FullStack Spring Semester 2025
   </div>
 </footer>
   );
